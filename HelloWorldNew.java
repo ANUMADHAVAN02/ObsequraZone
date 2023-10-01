d0356f938773723a973efd84bad308cbf067e687
ANUMADHAVAN02 <146543040+ANUMADHAVAN02@users.noreply.github.com>
2023-10-01 13:03:30 +0530

    Add files via upload

---
+++ HelloWorldNew.java
@@ -0,0 +1,9 @@
+class HelloWorldNew
+{
+public static void main(String args[])
+{
+/*System.out.println("HelloWorld");// single line
+System.out.println("HelloWorldnew");*/
+System.out.print("HelloWorldnewworld");
+}
+}
